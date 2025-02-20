1d20490a59194d315ff6b570993c9f7809596461
Flavia Mendes <flavinharamendes@gmail.com>
2025-02-20 11:57:13 -0300

    finished

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,27 @@
 import styles from '../styles/Home.module.css';
+import Header from '../componentes/Header';
 
-
-export default function Home (){
+export default function Home() {
     return (
-        <div className ={styles.container}>
-            <header className={styles.header}>
-                <h1 className={styles.title}>URSINHO TRAPALHÃO</h1>
-            </header>
-
+        <div className={styles.container}>
+            <Header>
+                Teste
+            </Header>
+            <main className={styles.main}>
+            <div className={styles.bloco}>
+                <div className={styles.article}>
+                    <div className={styles.card}>
+                    </div>
+                    <div className={styles.card}>
+                    </div>
+                    <div className={styles.card}>
+                    </div>
+                </div>
+                <aside className={styles.aside}>
+            <img className={styles.imagebear} src="/bear.png" />
+            </aside>
+            </div>
+            </main>
         </div>
     );
 }
