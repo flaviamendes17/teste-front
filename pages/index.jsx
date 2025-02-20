08e9ff95e1696459d3e45510999f2a213d09a5db
Flavia Mendes <flavinharamendes@gmail.com>
2025-02-20 10:22:09 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css';
+
+
+export default function Home (){
+    return (
+        <div className ={styles.container}>
+            <header className={styles.header}>
+                <h1 className={styles.title}>URSINHO TRAPALHÃO</h1>
+            </header>
+
+        </div>
+    );
+}
